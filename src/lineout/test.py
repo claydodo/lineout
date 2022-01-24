f3de9ac40c62c8231a63dbf64d91aa2aa299f9c6
yangdw <claydodo@foxmail.com>
2022-01-24 15:20:45 +0800

    APITestMixin add **extra to each api test method

--- src/lineout/test.py
+++ src/lineout/test.py
@@ -7,35 +7,35 @@ from django.test import Client
 
 
 class APITestMixin:
-    def assertApiAllowGet(self, url, params=None, client=None, msg=None):
+    def assertApiAllowGet(self, url, params=None, client=None, msg=None, **extra):
         if not client:
             client = Client()
 
-        res = client.get(url_add_params(url, params), content_type='application/json')
+        res = client.get(url_add_params(url, params), content_type='application/json', **extra)
         self.assertStatusCodeIs(res.status_code, 200, msg=msg)
         data = res.json()
         return data
 
-    def assertApiAllowPost(self, url, sample_data, params=None, client=None, msg=None):
+    def assertApiAllowPost(self, url, sample_data, params=None, client=None, msg=None, **extra):
         if not client:
             client = Client()
 
-        res = client.post(url_add_params(url, params), sample_data, content_type='application/json')
+        res = client.post(url_add_params(url, params), sample_data, content_type='application/json', **extra)
         self.assertStatusCodeIs(res.status_code, STATUS_PASSED, msg=msg)
         return res.json()
 
-    def assertApiForbidGet(self, url, params=None, client=None, msg=None):
+    def assertApiForbidGet(self, url, params=None, client=None, msg=None, **extra):
         if not client:
             client = Client()
 
-        res = client.get(url_add_params(url, params), content_type='application/json')
+        res = client.get(url_add_params(url, params), content_type='application/json', **extra)
         self.assertStatusCodeIs(res.status_code, STATUS_REJECTED, msg=msg)
 
-    def assertApiForbidPost(self, url, sample_data, params=None, client=None, msg=None):
+    def assertApiForbidPost(self, url, sample_data, params=None, client=None, msg=None, **extra):
         if not client:
             client = Client()
 
-        res = client.post(url_add_params(url, params), sample_data, content_type='application/json')
+        res = client.post(url_add_params(url, params), sample_data, content_type='application/json', **extra)
         self.assertStatusCodeIs(res.status_code, STATUS_REJECTED, msg=msg)
 
     def assertIsListResult(self, data, msg=None):
@@ -48,109 +48,109 @@ class APITestMixin:
         if not isinstance(data, dict) or 'results' not in data or not isinstance(data['results'], list):
             self.fail(self._formatMessage(msg, "Data is not paginated list api result: {}".format(data)))
 
-    def assertApiAllowList(self, url, params=None, client=None, msg=None):
+    def assertApiAllowList(self, url, params=None, client=None, msg=None, **extra):
         if not client:
             client = Client()
 
         final_url = url_add_params(url, params)
-        res = client.get(final_url, content_type='application/json')
+        res = client.get(final_url, content_type='application/json', **extra)
         self.assertStatusCodeIs(res.status_code, StatusCode.OK, msg=msg)
         data = res.json()
         return self.assertIsListResult(data, msg=msg)
 
-    def assertApiAllowRetrieve(self, url, sample_id, params=None, client=None, msg=None):
+    def assertApiAllowRetrieve(self, url, sample_id, params=None, client=None, msg=None, **extra):
         if not client:
             client = Client()
 
         final_url = url_add_params(get_detail_url(url, sample_id), params)
-        res = client.get(final_url)
+        res = client.get(final_url, content_type='application/json', **extra)
         self.assertStatusCodeIs(res.status_code, 200, msg=msg)
         data = res.json()
         self.assertIsInstance(data, dict, msg=msg)
         return data
 
-    def assertApiAllowCreate(self, url, sample_data, params=None, client=None, msg=None):
+    def assertApiAllowCreate(self, url, sample_data, params=None, client=None, msg=None, **extra):
         if not client:
             client = Client()
 
         final_url = url_add_params(url, params)
-        res = client.post(final_url, sample_data, content_type='application/json')
+        res = client.post(final_url, sample_data, content_type='application/json', **extra)
         self.assertStatusCodeIs(res.status_code, 201, msg=msg)
         return res.json()
 
-    def assertApiAllowUpdate(self, url, sample_id, sample_data, params=None, client=None, msg=None):
+    def assertApiAllowUpdate(self, url, sample_id, sample_data, params=None, client=None, msg=None, **extra):
         if not client:
             client = Client()
 
         final_url = url_add_params(get_detail_url(url, sample_id), params)
-        res = client.put(final_url, sample_data, content_type='application/json')
+        res = client.put(final_url, sample_data, content_type='application/json', **extra)
         self.assertStatusCodeIs(res.status_code, STATUS_PASSED, msg=msg)
         return res.json()
 
-    def assertApiAllowPartialUpdate(self, url, sample_id, sample_data, params=None, client=None, msg=None):
+    def assertApiAllowPartialUpdate(self, url, sample_id, sample_data, params=None, client=None, msg=None, **extra):
         if not client:
             client = Client()
 
         final_url = url_add_params(get_detail_url(url, sample_id), params)
-        res = client.patch(final_url, sample_data, content_type='application/json')
+        res = client.patch(final_url, sample_data, content_type='application/json', **extra)
         self.assertStatusCodeIs(res.status_code, STATUS_PASSED, msg=msg)
         return res.json()
 
-    def assertApiAllowDelete(self, url, sample_id, params=None, client=None, msg=None):
+    def assertApiAllowDelete(self, url, sample_id, params=None, client=None, msg=None, **extra):
         if not client:
             client = Client()
 
         final_url = url_add_params(get_detail_url(url, sample_id), params)
-        res = client.delete(final_url)
+        res = client.delete(final_url, **extra)
         self.assertStatusCodeIs(res.status_code, 204, msg=msg)
 
-    def assertApiForbidList(self, url, params=None, client=None, msg=None):
+    def assertApiForbidList(self, url, params=None, client=None, msg=None, **extra):
         if not client:
             client = Client()
 
         final_url = url_add_params(url, params)
-        res = client.get(final_url, content_type='application/json')
+        res = client.get(final_url, content_type='application/json', **extra)
         self.assertStatusCodeIs(res.status_code, STATUS_REJECTED, msg=msg)
 
-    def assertApiForbidRetrieve(self, url, sample_id, params=None, client=None, msg=None):
+    def assertApiForbidRetrieve(self, url, sample_id, params=None, client=None, msg=None, **extra):
         if not client:
             client = Client()
 
         final_url = url_add_params(get_detail_url(url, sample_id), params)
-        res = client.get(final_url)
+        res = client.get(final_url, content_type='application/json', **extra)
         self.assertStatusCodeIs(res.status_code, STATUS_REJECTED, msg=msg)
 
-    def assertApiForbidCreate(self, url, sample_data, params=None, client=None, msg=None):
+    def assertApiForbidCreate(self, url, sample_data, params=None, client=None, msg=None, **extra):
         if not client:
             client = Client()
 
         final_url = url_add_params(url, params)
-        res = client.post(final_url, sample_data, content_type='application/json')
+        res = client.post(final_url, sample_data, content_type='application/json', **extra)
         self.assertStatusCodeIs(res.status_code, STATUS_REJECTED, msg=msg)
 
-    def assertApiForbidUpdate(self, url, sample_id, sample_data, params=None, client=None, msg=None):
+    def assertApiForbidUpdate(self, url, sample_id, sample_data, params=None, client=None, msg=None, **extra):
         if not client:
             client = Client()
 
         final_url = url_add_params(get_detail_url(url, sample_id), params)
-        res = client.put(final_url, sample_data, content_type='application/json')
+        res = client.put(final_url, sample_data, content_type='application/json', **extra)
         self.assertStatusCodeIs(res.status_code, STATUS_REJECTED, msg=msg)
 
-        res = client.patch(final_url, sample_data, content_type='application/json')
+        res = client.patch(final_url, sample_data, content_type='application/json', **extra)
         self.assertStatusCodeIs(res.status_code, STATUS_REJECTED, msg=msg)
 
-    def assertApiForbidDelete(self, url, sample_id, params=None, client=None, msg=None):
+    def assertApiForbidDelete(self, url, sample_id, params=None, client=None, msg=None, **extra):
         if not client:
             client = Client()
 
         final_url = url_add_params(get_detail_url(url, sample_id), params)
-        res = client.delete(final_url)
+        res = client.delete(final_url, **extra)
         self.assertStatusCodeIs(res.status_code, STATUS_REJECTED, msg=msg)
 
-    def assertApiReadOnly(self, url, sample_id, sample_data, params=None, client=None, msg=None):
-        self.assertApiForbidCreate(url, sample_data=sample_data, params=params, client=client, msg=msg)
-        self.assertApiForbidUpdate(url, sample_id=sample_id, sample_data=sample_data, params=params, client=client, msg=msg)
-        self.assertApiForbidDelete(url, sample_id=sample_id, params=params, client=client, msg=msg)
+    def assertApiReadOnly(self, url, sample_id, sample_data, params=None, client=None, msg=None, **extra):
+        self.assertApiForbidCreate(url, sample_data=sample_data, params=params, client=client, msg=msg, **extra)
+        self.assertApiForbidUpdate(url, sample_id=sample_id, sample_data=sample_data, params=params, client=client, msg=msg, **extra)
+        self.assertApiForbidDelete(url, sample_id=sample_id, params=params, client=client, msg=msg, **extra)
 
     def assertStatusCodeIs(self, status_code, code_or_group, msg=None):
         if isinstance(code_or_group, int):
